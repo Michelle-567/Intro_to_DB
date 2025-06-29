749d390218ab12a6a2069c3c8ad1f3e4e6b28b40
Michelle-567 <michellendirangu2001@gmail.com>
2025-06-29 23:25:02 +0300

    Fix: Add CREATE DATABASE line to SQL file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,11 +30,12 @@ CREATE TABLE Orders (
 );
 );
 
-CREATE TABLE Order_details (
+CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+
 );
